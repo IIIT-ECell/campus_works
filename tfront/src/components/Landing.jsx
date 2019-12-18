d6ccc6dd6ab82ee1a2d42bf99ed908a43894bf7b
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-12-18 15:36:51 +0530

    Added an image to home

--- tfront/src/components/Landing.jsx
+++ tfront/src/components/Landing.jsx
@@ -1,20 +1,23 @@
 import React, { Component} from 'react';
 import './Landing.css'
 import {Button, ButtonToolbar} from 'react-bootstrap';
+import Hero from '../assets/hero.png'
+
 class Landing extends Component{
     render(){
         return(
             <div id="intro" className="vh-100">
                 <div className="mask d-flex justify-content-center align-items-center">
-                    <div className="container text-center">
+                    <div className="container d-flex text-center">
                         <div className="row bg-white p-5 rounded">
-                            <div className="col-md-12">
+                            <div className="col-md-6 my-auto">
                                 <h2 className="display-4 font-weight-bold">Campus Works</h2>
-                                <hr className="hr-light"/>
-                                <h4 className="my-4">INTERNSHIPS MADE EASY</h4>
-                                <Button variant="outline-dark" size="lg" href="/register/company">Register as Company</Button>
+                                <h5>Connecting Students with Startups. Internships have never been easier</h5>
+                                <Button variant="btn btn-dark" size="lg" href="/register/company">Register as Company</Button>
                                 <br className="my-4"/>
-                                <Button variant="outline-dark" size="lg" disabled>Register as Student</Button>
+                            </div>
+                            <div className="order-lg-first w-50 mx-auto">
+                                <img className="img-fluid" src={ Hero }/>
                             </div>
                         </div>
                     </div>
