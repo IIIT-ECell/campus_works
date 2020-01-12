af227bb71f5c5350362c6b1867505644e2c4c32e
YoogottamK <yoogottamk@outlook.com>
2020-01-12 20:10:16 +0530

    Added edit link to company job list

--- tfront/src/components/CompanyHome.jsx
+++ tfront/src/components/CompanyHome.jsx
@@ -1,34 +1,35 @@
 import React, {Component} from 'react';
 import {Table, Button} from 'react-bootstrap';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-class CompanyHome extends Component{
+class CompanyHome extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {jobs:[]};
+        this.state = {jobs: []};
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios({
-            method:"POST",
-            url:"http://localhost:8000/jobs",
-            data:{
-                "token":localStorage.getItem("token"),
+            method: "POST",
+            url: "http://localhost:8000/jobs",
+            data: {
+                "token": localStorage.getItem("token"),
             },
-            headers:{
+            headers: {
                 'Content-Type': 'application/json',
             }
         })
-        .then((res)=>{
-            console.log(res);
-            this.setState({"jobs":res.data});
-            console.log(this.state);
-        });
+            .then((res) => {
+                console.log(res);
+                this.setState({"jobs": res.data});
+                console.log(this.state);
+            });
     }
 
 
-    render(){
+    render() {
         return (
             <Table>
                 <thead>
@@ -36,22 +37,23 @@ class CompanyHome extends Component{
                         <th colSpan="6">Jobs posted</th>
                     </tr>
                     <tr>
-                        <th>#</th>
                         <th>Job Name</th>
                         <th>Description</th>
                         <th>Start Date</th>
                         <th>Skill</th>
                         <th>Stipend</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.jobs && this.state.jobs.map((item,key)=>{
+                    {this.state.jobs && this.state.jobs.map((item, key) => {
                         return (<tr>
                             <td>{item.fields.job_name}</td>
                             <td>{item.fields.description}</td>
                             <td>{item.fields.start_date}</td>
                             <td>{item.fields.skill}</td>
                             <td>{item.fields.stipend}</td>
+                            <td><Link to={"/jobs/edit/" + item.pk}>Edit</Link></td>
                         </tr>)
                     })}
                 </tbody>
