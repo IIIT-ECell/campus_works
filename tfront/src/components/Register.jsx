c226622065707698a18ad588da53b21e56c10403
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-01-25 22:57:18 +0530

    changed register page

--- tfront/src/components/Register.jsx
+++ tfront/src/components/Register.jsx
@@ -1,23 +1,15 @@
 import React,{Component} from 'react';
+import Landing from './Landing';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Nav1 from './Nav1';
 
 class RegisterForm extends Component{
     render(){
         return(
-            <div className="vh-100 d-flex justify-content-center align-items-center">
-                <div className="row my-auto">
-                    <div className="col-12 text-center">
-                        <Link to="/register/company">
-                        <Button>Register Company</Button>
-                        </Link>
-                    </div>
-                    <div className="col-12 text-center">
-                        <Link to="/register/student">
-                        <Button>Register Student</Button>
-                        </Link>
-                    </div>
-                </div>
+            <div>
+                <Nav1></Nav1>
+                <Landing/>
             </div>
         )
     }
