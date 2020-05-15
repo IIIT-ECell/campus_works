0134dbb921890ce4eddec1de7b15086466f5868e
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-05-16 00:45:26 +0530

    student profile looks better

--- tfront/src/components/StudentProfile.jsx
+++ tfront/src/components/StudentProfile.jsx
@@ -1,5 +1,5 @@
 import React,{ Component } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Row } from "react-bootstrap";
 import NavStudent from './NavStudent';
 import axios from "axios";
 import NavCompany from "./NavCompany";
@@ -73,48 +73,52 @@ class StudentProfile extends Component{
                 <div>
                 {localStorage.getItem('type')==1 && <NavStudent></NavStudent>}
                 {localStorage.getItem('type')==2 && <NavCompany></NavCompany>}
-                <div className="d-flex justify-content-center align-items-center">
+                <div className="container-flex justify-content-center align-items-center">
                     <div className="my-auto">
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Group controlId="formName">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" id="name" disabled onChange={this.handleChange} placeholder="Name" value={this.state.user.first_name}/>
-                        </Form.Group>
-                        <Form.Group controlId="formEmail">
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" id="email" disabled onChange={this.handleChange} placeholder="Email" value={this.state.user.email}/>
-                        </Form.Group>
-                        <Form.Group controlId="formPhoneNumber">
-                            <Form.Label>Phone Number</Form.Label>
-                            <Form.Control type="number" id="phone_number" disabled onChange={this.handleChange} placeholder="Phone Number" value={this.state.student.phone_number}/>
-                        </Form.Group>
-                        <Form.Group controlId="formRollNumber">
-                            <Form.Label>Roll Number</Form.Label>
-                            <Form.Control type="number" id="student_id" disabled onChange={this.handleChange} placeholder="Roll Number" value={this.state.student.student_id}/>
-                        </Form.Group>
-                        <Form.Group controlId="formStudy">
-                            <Form.Label>Year of Study</Form.Label>
-                            <Form.Control as="select" id="year_of_study" disabled onChange={this.handleChange} placeholder="Gender" value={this.state.student.year_of_study}>
-                            <option value="1">First Year Undergrad</option>
-                            <option value="2">Second Year Undergrad</option>
-                            <option value="3">Third Year Undergrad</option>
-                            <option value="4">Fourth Year Undergrad</option>
-                            <option value="5">Postgrads (5th year DD, PG+)</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="formGender">
-                            <Form.Label>Gender</Form.Label>
-                            <Form.Control as="select" id="gender" disabled onChange={this.handleChange} value={this.state.student.gender}>
-                                <option value="M">Male</option>
-                                <option value="F">Female</option>
-                                <option value="O">Other</option>
-                                <option value="N">Prefer not to say</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="formResume">
+                    <Form onSubmit={this.handleSubmit} className="container p-5">
+                        <div className="row">
+                            <Form.Group controlId="formName" className="col-md-4">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" id="name" disabled onChange={this.handleChange} placeholder="Name" value={this.state.user.first_name}/>
+                            </Form.Group>
+                            <Form.Group controlId="formEmail" className="col-md-4">
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control type="email" id="email" disabled onChange={this.handleChange} placeholder="Email" value={this.state.user.email}/>
+                            </Form.Group>
+                            <Form.Group controlId="formPhoneNumber" className="col-md-4">
+                                <Form.Label>Phone Number</Form.Label>
+                                <Form.Control type="number" id="phone_number" disabled onChange={this.handleChange} placeholder="Phone Number" value={this.state.student.phone_number}/>
+                            </Form.Group>
+                        </div>
+                        <Row>
+                            <Form.Group controlId="formRollNumber" className="col-md-4">
+                                <Form.Label>Roll Number</Form.Label>
+                                <Form.Control type="number" id="student_id" disabled onChange={this.handleChange} placeholder="Roll Number" value={this.state.student.student_id}/>
+                            </Form.Group>
+                            <Form.Group controlId="formStudy" className="col-md-4">
+                                <Form.Label>Year of Study</Form.Label>
+                                <Form.Control as="select" id="year_of_study" disabled onChange={this.handleChange} placeholder="Gender" value={this.state.student.year_of_study}>
+                                <option value="1">First Year Undergrad</option>
+                                <option value="2">Second Year Undergrad</option>
+                                <option value="3">Third Year Undergrad</option>
+                                <option value="4">Fourth Year Undergrad</option>
+                                <option value="5">Postgrads (5th year DD, PG+)</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="formGender" className="col-md-4">
+                                <Form.Label>Gender</Form.Label>
+                                <Form.Control as="select" id="gender" disabled onChange={this.handleChange} value={this.state.student.gender}>
+                                    <option value="M">Male</option>
+                                    <option value="F">Female</option>
+                                    <option value="O">Other</option>
+                                    <option value="N">Prefer not to say</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Row>
+                        <Form.Group controlId="formResume" className="col-md-4">
                             <a href={"https://campusworks.pythonanywhere.com/resume?id="+this.props.match.params.student_id} target="_blank">Resume Link</a>
                         </Form.Group>
-                        {this.state.formSubmitted===false && <button type="submit" className="btn btn-dark w-100" onClick={this.handleSubmit}>Submit</button>}
+                        {this.state.formSubmitted===false && <p className="col-md-4 offset-md-4"><button type="submit" className="btn btn-dark w-100" onClick={this.handleSubmit}>Submit</button></p>}
                      </Form>
                     </div>
                 </div>
