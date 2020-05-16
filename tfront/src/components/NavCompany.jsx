aa8740b53c6f8b8d74090456503bda51bf570952
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2020-05-16 22:46:39 +0530

    Changed company profile

--- tfront/src/components/NavCompany.jsx
+++ tfront/src/components/NavCompany.jsx
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import axios from 'axios'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class NavCompany extends Component{
@@ -41,7 +41,10 @@ class NavCompany extends Component{
                     <Nav className="ml-auto text-white">
                         <Nav.Link><Link to={"/company/home"} className="text-white"><FontAwesomeIcon icon="home"/> Home</Link></Nav.Link>
                         <Nav.Link><Link to={"/login"} className="text-white"><FontAwesomeIcon icon="sign-out-alt"/> Logout</Link></Nav.Link>
-                        <Nav.Link><Link to={"/company/profile/"+this.state.company.pk} className="text-white"><FontAwesomeIcon icon="user"/> Profile</Link></Nav.Link>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown" className="mr-auto" alignRight variant="dark" bg="dark">
+                            <NavDropdown.Item><Link to={"/company/profile/"+this.state.company.pk}><FontAwesomeIcon icon="user"/> View Profile</Link></NavDropdown.Item>
+                            <NavDropdown.Item><Link to={"/change-password/"}><FontAwesomeIcon icon="user-edit"/> Change Password</Link></NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
