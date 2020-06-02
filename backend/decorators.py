e914fd14e7b7e4626e117dc07556e36d2dec4669
yoogottamk <yoogottamk@outlook.com>
2020-06-02 13:29:24 +0530

    minor older edits

--- backend/decorators.py
+++ backend/decorators.py
@@ -1,25 +1,29 @@
 from rest_framework.authtoken.models import Token
 from backend.models import *
 
+
 def student_required(key):
     token = Token.objects.get(key=key)
     user = CustomUser.objects.get(id=token.user_id)
-    if(user.user_type==1):
+    if user.user_type == 1:
         return True
     return False
+
 
 def company_required(key):
     token = Token.objects.get(key=key)
     user = CustomUser.objects.get(id=token.user_id)
-    if(user.user_type == 2):
+    if user.user_type == 2:
         return True
     return False
+
 
 def get_student_id(key):
     token = Token.objects.get(key=key)
     student = Student.objects.get(user_id=token.user_id)
     print(student.id)
     return student.id
+
 
 def get_company_id(key):
     token = Token.objects.get(key=key)
